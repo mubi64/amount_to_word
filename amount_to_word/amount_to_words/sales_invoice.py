a7faf0e0caf656a39db7a38f8a0639006d29f6e7
saadsafda <muhammadsaadsafdar2005@gmail.com>
2023-01-05 15:03:55 +0500

    resolve the issue

--- amount_to_word/amount_to_words/sales_invoice.py
+++ amount_to_word/amount_to_words/sales_invoice.py
@@ -2,6 +2,8 @@ import frappe
 from frappe import _
 from amount_to_word.amount_to_words.api import convert_amount_to_words
 
+
 def before_save(doc, method):
-    amount = doc.rounded_total if doc.rounded_total is not None else doc.grand_total
+    amount = doc.rounded_total if (
+        doc.rounded_total is not None and doc.rounded_total > 0) else doc.grand_total
     doc.in_words_arabic = convert_amount_to_words(amount, 'ar', 'SAR')
