0a0bb982b2cf70f08f0cd93b83aa836ed30af3b5
sanaullah <sanaullahsattar122@gmail.com>
2025-11-13 11:06:35 +0500

    Delivery note amount to words issue.add customize field

--- amount_to_word/amount_to_words/delivery_note.py
+++ amount_to_word/amount_to_words/delivery_note.py
@@ -4,5 +4,10 @@ from amount_to_word.amount_to_words.api import get_df_currency, convert_amount_t
 
 def before_save(doc, method):
     currency = get_df_currency(doc.company)
-    amount = doc.rounded_total if (doc.rounded_total is not None and doc.rounded_total > 0) else doc.grand_total
-    doc.in_words_arabic = convert_amount_to_words(amount, 'ar', 'SAR')
+    # amount = doc.rounded_total if (doc.rounded_total is not None and doc.rounded_total > 0) else doc.grand_total
+   
+    # doc.in_words_arabic = convert_amount_to_words(amount, 'ar', 'SAR')
+    amount = doc.grand_total if (doc.grand_total is not None and doc.grand_total > 0) else 0
+
+    # Convert exact amount to Arabic words
+    doc.in_words_arabic = convert_amount_to_words(amount, 'ar', currency[0].get("currency"))
